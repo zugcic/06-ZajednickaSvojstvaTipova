67737a4ad82ffb89fe8ee4c8acde80df931287a6
Julijan Šribar <julijan.sribar@gmail.com>
2016-11-01 13:28:44 +0100

    Popravljen projekt MetodaEquals i dodani testovi.

--- MetodaEquals/Equals.cs
+++ MetodaEquals/Equals.cs
@@ -10,22 +10,27 @@ namespace Vsite.CSharp
     {
     }
 
-    class Program
+    public class MetodaEquals
     {
-        static void Main(string[] args)
+        public static void UsporedbaKlasa()
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = null;
 
             // TODO: Usporediti objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.Equals te ispisati rezultat
-
+            Console.WriteLine(MojaKlasa.Equals(mk1, mk2));
 
             // TODO: Usporediti objekte mk1 i mk2 pozivom metode mk1.Equals te ispisati rezultat
+            Console.WriteLine(mk1.Equals(mk2));
 
 
             // TODO: Usporediti objekte mk1 i mk2 pozivom metode mk2.Equals te ispisati rezultat
+            Console.WriteLine(mk2.Equals(mk1));
 
+        }
 
+        public static void UsporedbaStruktura()
+        {
             // TODO: Storiti dvije strukture ms1 i ms2 na isti način kao i za gornje instance klasa mk1 i mk2
 
 
@@ -37,9 +42,17 @@ namespace Vsite.CSharp
 
             // TODO: Usporediti objekte ms1 i ms2 pozivom metode ms2.Equals te ispisati rezultat
 
+        }
 
-            // TODO: Ispis prekopirati u datoteku Equals.txt pridruženu ovom projektu te obrazložite svaki rezultat
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usporedba klasa");
+            UsporedbaKlasa();
 
+            Console.WriteLine("Usporedba struktura");
+            UsporedbaStruktura();
+
+            Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
         }
     }
---
+++ Testovi/TestEquals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.CSharp;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestEquals : ConsoleTest
+    {
+        [TestMethod]
+        public void Equals_UsporedbaKlasa()
+        {
+            try
+            {
+                MetodaEquals.UsporedbaKlasa();
+            }
+            catch (NullReferenceException)
+            {
+            }
+
+            Assert.AreEqual(false, cw.GetBoolean());
+            Assert.AreEqual(false, cw.GetBoolean());
+            Assert.IsTrue(cw.IsEmpty);
+        }
+    }
+}
