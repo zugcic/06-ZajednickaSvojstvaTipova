a973099ebd7281f247b9839e45c62bfeffdf83b7
Julijan Šribar <julijan.sribar@gmail.com>
2016-11-01 14:45:07 +0100

    Popravljen projekt MetodaEqualsZaReferentniTip i dodani testovi.

---
+++ MetodaEqualsZaReferentiTip/MetodaEqualsZaReferentniTip.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Vsite.CSharp
+{
+    class Osoba
+    {
+        public Osoba(string ime, int matičniBroj)
+        {
+            m_ime = ime;
+            m_matičniBroj = matičniBroj;
+        }
+
+        string m_ime;       // član referentnog tipa
+        int m_matičniBroj;  // član vrijednosnog tipa
+
+        // TODO: Pregaziti (override) metodu Equals tako da za osobe s istim imenom i istim matičnim brojem rezultat bude true
+
+
+        public override string ToString()
+        {
+            return string.Format("'{0}, {1}'", m_ime, m_matičniBroj);
+        }
+    }
+
+    public class MetodaEqualsZaReferentniTip
+    {
+        public static void UsporedbaReferenciNaIstiObjekt()
+        {
+            Osoba osobaA = new Osoba("Janko", 1);
+            Osoba osobaB = osobaA;
+
+            // TODO: Ispisati rezultat poziva metode osobaA.Equals(osobaB)
+            Console.WriteLine(osobaA.Equals(osobaB));
+
+            // TODO: Ispisati rezultat poziva metode ReferenceEquals(osobaB)
+            Console.WriteLine(object.ReferenceEquals(osobaA, osobaB));
+        }
+
+        public static void UsporedbaOsobaSRazličitimImenimaRazličitimMatičnimBrojem()
+        {
+            Osoba osobaA = new Osoba("Janko", 1);
+            Osoba osobaB = new Osoba("Darko", 2);
+
+            // TODO: Ispisati rezultat poziva metode osobaA.Equals(osobaB)
+            Console.WriteLine(osobaA.Equals(osobaB));
+
+            // TODO: Ispisati rezultat poziva metode ReferenceEquals(osobaB)
+            Console.WriteLine(object.ReferenceEquals(osobaA, osobaB));
+        }
+
+        public static void UsporedbaOsobaSIstimImenimaRazličtitimMatičnimBrojem()
+        {
+            Osoba osobaA = new Osoba("Janko", 1);
+            Osoba osobaB = new Osoba("Janko", 2);
+
+            // TODO: Ispisati rezultat poziva metode osobaA.Equals(osobaB)
+            Console.WriteLine(osobaA.Equals(osobaB));
+
+            // TODO: Ispisati rezultat poziva metode ReferenceEquals(osobaB)
+            Console.WriteLine(object.ReferenceEquals(osobaA, osobaB));
+        }
+
+        public static void UsporedbaOsobaSIstimImenimaIstimMatičnimBrojem()
+        {
+            Osoba osobaA = new Osoba("Janko", 5);
+            Osoba osobaB = new Osoba("Janko", 5);
+
+            // TODO: Ispisati rezultat poziva metode osobaA.Equals(osobaB)
+            Console.WriteLine(osobaA.Equals(osobaB));
+
+            // TODO: Ispisati rezultat poziva metode ReferenceEquals(osobaB)
+            Console.WriteLine(object.ReferenceEquals(osobaA, osobaB));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usporedba referenci na isti bojekt:");
+            UsporedbaReferenciNaIstiObjekt();
+
+            Console.WriteLine("Usporedba dviju osoba s različitim imenima i matičnim brojevima:");
+            UsporedbaOsobaSRazličitimImenimaRazličitimMatičnimBrojem();
+
+            Console.WriteLine("Usporedba dviju osoba s isitim imenima i različitim matičnim brojevima:");
+            UsporedbaOsobaSIstimImenimaRazličtitimMatičnimBrojem();
+
+            Console.WriteLine("Usporedba dviju osoba s isitim imenima i istim matičnim brojevima:");
+            UsporedbaOsobaSIstimImenimaIstimMatičnimBrojem();
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Testovi/TestEqualsZaReferentniTip.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.CSharp;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestEqualsZaReferentniTip : ConsoleTest
+    {
+        [TestMethod]
+        public void EqualsZaReferentniTip_UsporedbaReferenciNaIstiObjektUvijekVraćaTrue()
+        {
+            MetodaEqualsZaReferentniTip.UsporedbaReferenciNaIstiObjekt();
+            Assert.IsTrue(cw.GetBoolean());
+            Assert.IsTrue(cw.GetBoolean());
+        }
+
+        [TestMethod]
+        public void EqualsZaReferentniTip_UsporedbaDvijuOsobaSRazličitimImenimaRazličitimMatičnimBrojemVraćaFalse()
+        {
+            MetodaEqualsZaReferentniTip.UsporedbaOsobaSRazličitimImenimaRazličitimMatičnimBrojem();
+            Assert.IsFalse(cw.GetBoolean());
+            Assert.IsFalse(cw.GetBoolean());
+        }
+
+        [TestMethod]
+        public void EqualsZaReferentniTip_UsporedbaDvijuOsobaSIstimImenimaRazličtitimMatičnimBrojemVraćaFalse()
+        {
+            MetodaEqualsZaReferentniTip.UsporedbaOsobaSRazličitimImenimaRazličitimMatičnimBrojem();
+            Assert.IsFalse(cw.GetBoolean());
+            Assert.IsFalse(cw.GetBoolean());
+        }
+
+        [TestMethod]
+        public void EqualsZaReferentniTip_UsporedbaDvijuOsobaSIstimImenimaIstimMatičnimBrojemVraćaTrue()
+        {
+            MetodaEqualsZaReferentniTip.UsporedbaOsobaSIstimImenimaIstimMatičnimBrojem();
+            Assert.IsTrue(cw.GetBoolean());
+            Assert.IsFalse(cw.GetBoolean());
+        }
+    }
+}
