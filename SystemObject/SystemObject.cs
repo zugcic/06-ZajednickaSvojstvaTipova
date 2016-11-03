ff3674b4463e9d5cc999cc5443a7ce2e0d04dac5
Stjepan Zugcic <stzugcic@vsite.hr>
2016-11-03 20:46:27 +0100

    Riješili smo SystemObject()

--- SystemObject/SystemObject.cs
+++ SystemObject/SystemObject.cs
@@ -3,57 +3,65 @@
 namespace Vsite.CSharp
 {
     // TODO: Definirati klasu MojaKlasa bez ikakvih članova, samo s praznom definicijom.
-
+    class MojaKlasa {
+        
+    }
 
     public class SystemObject
     {
         public static void IspisToString()
         {
-            // TODO: Otkomentirati donje naredbe.
-            //MojaKlasa mk1 = new MojaKlasa();
-            //MojaKlasa mk2 = mk1;
-            //MojaKlasa mk3 = new MojaKlasa();
+            // Otkomentirati donje naredbe.
+            MojaKlasa mk1 = new MojaKlasa();
+            MojaKlasa mk2 = mk1;
+            MojaKlasa mk3 = new MojaKlasa();
 
             // TODO: Za svaki objekt (mk1, mk2, mk3) pozvati njegovu metodu ToString i ispisati na konzolu ono što ona vraća.
-
+            Console.WriteLine(mk1.ToString());
+            Console.WriteLine(mk2.ToString());
+            Console.WriteLine(mk3.ToString());
         }
 
         public static void IspisGetType()
         {
-            // TODO: Otkomentirati donje naredbe.
-            //MojaKlasa mk1 = new MojaKlasa();
-            //MojaKlasa mk2 = mk1;
-            //MojaKlasa mk3 = new MojaKlasa();
+            // Otkomentirati donje naredbe.
+            MojaKlasa mk1 = new MojaKlasa();
+            MojaKlasa mk2 = mk1;
+            MojaKlasa mk3 = new MojaKlasa();
 
             // TODO: Za svaki objekt (mk1, mk2, mk3) pozvati njegovu metodu GetType i ispisati na konzolu ono što ona vraća.
-
+            Console.WriteLine(mk1.GetType());
+            Console.WriteLine(mk2.GetType());
+            Console.WriteLine(mk3.GetType());
         }
 
         public static void IspisGetHashCode()
         {
-            // TODO: Otkomentirati donje naredbe.
-            //MojaKlasa mk1 = new MojaKlasa();
-            //MojaKlasa mk2 = mk1;
-            //MojaKlasa mk3 = new MojaKlasa();
+            // Otkomentirati donje naredbe.
+            MojaKlasa mk1 = new MojaKlasa();
+            MojaKlasa mk2 = mk1;
+            MojaKlasa mk3 = new MojaKlasa();
 
             // TODO: Za svaki objekt (mk1, mk2, mk3) pozvati njegovu metodu GetHashCode i ispisati na konzolu ono što ona vraća.
-
+            Console.WriteLine(mk1.GetHashCode()); //vraća integer koji jednoznačno identificira objekt
+            Console.WriteLine(mk2.GetHashCode()); //dva različita objekta bi trebala imati različite vrijednosti GetHashCode()
+            Console.WriteLine(mk3.GetHashCode());
         }
 
         public static void IspisEquals()
         {
-            // TODO: Otkomentirati donje naredbe.
-            //MojaKlasa mk1 = new MojaKlasa();
-            //MojaKlasa mk2 = mk1;
-            //MojaKlasa mk3 = new MojaKlasa();
+            // Otkomentirati donje naredbe.
+            MojaKlasa mk1 = new MojaKlasa();
+            MojaKlasa mk2 = mk1;
+            MojaKlasa mk3 = new MojaKlasa();
 
-            // TODO: Otkomentirati donje naredbe i ispisati na konzolu rezultate usporedbi.
-            //Console.WriteLine(mk1.Equals(mk2));
-            //Console.WriteLine(mk1.Equals(mk3));
-            //Console.WriteLine(mk2.Equals(mk1));
-            //Console.WriteLine(mk2.Equals(mk3));
-            //Console.WriteLine(mk3.Equals(mk1));
-            //Console.WriteLine(mk3.Equals(mk2));
+            //  Otkomentirati donje naredbe i ispisati na konzolu rezultate usporedbi.
+            Console.WriteLine(mk1.Equals(mk2));//radi usporedbu po referencama
+            Console.WriteLine(mk1.Equals(mk3));
+            Console.WriteLine(mk2.Equals(mk1));
+            Console.WriteLine(mk2.Equals(mk3));
+            Console.WriteLine(mk3.Equals(mk1));
+            Console.WriteLine(mk3.Equals(mk2));
         }
 
         static void Main(string[] args)
